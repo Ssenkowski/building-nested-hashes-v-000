98eb6767d1a7c9e730bf37175c39302057902382
Steven Senkowski <scsenkowski@gmail.com>
2017-11-13 22:47:31 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,7 +20,7 @@ def third_challenge
         name: "Lady Montague", age: "51"
         },
       :heroine => {
-        name: "Juliet", age: "15"  
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
